9828f93ae63087d0a69a63aab4669c1fdd999b35
seeo <aragonlotr5@gmail.com>
2019-05-27 19:15:08 +0800

    inserted Amplify.configure in the App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Navigator, Main } from './components';
 import './App.css';
 
 import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
 
+Amplify.configure(awsmobile);
 
 class App extends Component {
   render() {
